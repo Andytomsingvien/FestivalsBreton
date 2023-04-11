a0f3d3df5f78659c4ccf7429bcaaa0332dc8dfa6
Andy <121941283+Andytomsingvien@users.noreply.github.com>
2023-04-11 12:22:27 +0200

    Ajout de la fonction ajouterFestival

--- src/main/java/fr/simplon/festivalsbreton/festivalsbreton/controller/FestivalController.java
+++ src/main/java/fr/simplon/festivalsbreton/festivalsbreton/controller/FestivalController.java
@@ -1,5 +1,32 @@
 package fr.simplon.festivalsbreton.festivalsbreton.controller;
 
-public class FestivalController {
+import fr.simplon.festivalsbreton.festivalsbreton.dao.FestivalDao;
+import fr.simplon.festivalsbreton.festivalsbreton.entity.Festival;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.sql.Date;
+
+@Controller
+public class FestivalController {
+    @Autowired
+    private FestivalDao festivalDao;
+
+    @RequestMapping("/form")
+    public String endPointForm(Model model) {
+        return "form";
+    }
+    @PostMapping("/submit")
+    public String ajouterFestival(@ModelAttribute("festival") Festival festival, Model model) {
+        model.addAttribute("festival", festival);
+        festivalDao.saveFestival(festival.getNom(),festival.getVille(),festival.getCp(),festival.getLieu(),festival.getDebut(),festival.getFin(),festival.getLat(),festival.getLon());
+        return "index";
+    }
 }
+
+
--- src/main/java/fr/simplon/festivalsbreton/festivalsbreton/entity/Festival.java
+++ src/main/java/fr/simplon/festivalsbreton/festivalsbreton/entity/Festival.java
@@ -2,7 +2,7 @@ package fr.simplon.festivalsbreton.festivalsbreton.entity;
 
 import jakarta.persistence.*;
 
-import java.util.Date;
+import java.sql.Date;
 
 @Entity
 @Table(name = "Festivals")
