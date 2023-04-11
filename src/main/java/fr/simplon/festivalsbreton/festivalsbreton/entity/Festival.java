c9334ee2f65f06ae494481fffac1b523054ff2d1
Andy <121941283+Andytomsingvien@users.noreply.github.com>
2023-04-11 10:35:53 +0200

    Mise à jours de Festival avant le fork ou nous allons split le travail

--- src/main/java/fr/simplon/festivalsbreton/festivalsbreton/entity/Festival.java
+++ src/main/java/fr/simplon/festivalsbreton/festivalsbreton/entity/Festival.java
@@ -1,9 +1,108 @@
 package fr.simplon.festivalsbreton.festivalsbreton.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "Festivals")
 public class Festival {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "nom")
+    private String nom;
+
+    @Column(name = "ville")
+    private String ville;
+
+    @Column(name = "cp")
+    private Integer cp;
+
+    @Column(name = "lieu")
+    private String lieu;
+
+    @Column(name = "debut")
+    private Date debut;
+
+    @Column(name = "fin")
+    private Date fin;
+
+    @Column(name = "lat")
+    private Double lat;
+
+    @Column(name = "lon")
+    private Double lon;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public Integer getCp() {
+        return cp;
+    }
+
+    public void setCp(Integer cp) {
+        this.cp = cp;
+    }
+
+    public String getLieu() {
+        return lieu;
+    }
+
+    public void setLieu(String lieu) {
+        this.lieu = lieu;
+    }
+
+    public Date getDebut() {
+        return debut;
+    }
+
+    public void setDebut(Date debut) {
+        this.debut = debut;
+    }
+
+    public Date getFin() {
+        return fin;
+    }
+
+    public void setFin(Date fin) {
+        this.fin = fin;
+    }
+
+    public Double getLat() {
+        return lat;
+    }
+
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+
+    public Double getLon() {
+        return lon;
+    }
+
+    public void setLon(Double lon) {
+        this.lon = lon;
+    }
 }
