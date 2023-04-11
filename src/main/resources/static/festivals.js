c73a0859db95e341b1e237b3cb3bbf7842009b86
SolC <solenne.cap@isen-ouest.yncrea.fr>
2023-04-11 15:48:47 +0200

    ajout des boutons

--- src/main/resources/static/festivals.js
+++ src/main/resources/static/festivals.js
@@ -1,6 +1,17 @@
+fetch('http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=astro&output=json').then(response => {
+    return response.json()
+}).then(data => {
+    console.log(data)
+}).catch(err => {
+    console.error(err)
+})
 
     function ajouterFestival() {
     // Fonction à exécuter lors du clic sur le bouton
     // Rediriger vers la page "nouveau festival"
     window.location.href = "nouveauFestival.html";
 }
+
+
+
+
