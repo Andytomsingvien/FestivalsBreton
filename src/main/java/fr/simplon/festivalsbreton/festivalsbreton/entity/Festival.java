5e80055adf6f6e2ba29718525b4dc760ae76355a
Andy <121941283+Andytomsingvien@users.noreply.github.com>
2023-04-11 10:25:53 +0200

    Test de la mise à jour de git avec @Entity et @Table

--- src/main/java/fr/simplon/festivalsbreton/festivalsbreton/entity/Festival.java
+++ src/main/java/fr/simplon/festivalsbreton/festivalsbreton/entity/Festival.java
@@ -1,4 +1,9 @@
 package fr.simplon.festivalsbreton.festivalsbreton.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Festivals")
 public class Festival {
 }
