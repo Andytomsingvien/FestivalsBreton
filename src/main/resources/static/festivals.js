493d93b8a61331498dbd3a8f475d2226148a7f9c
SolC <solenne.cap@isen-ouest.yncrea.fr>
2023-04-11 12:20:53 +0200

    index.html

---
+++ src/main/resources/static/festivals.js
@@ -0,0 +1,6 @@
+
+    function ajouterFestival() {
+    // Fonction à exécuter lors du clic sur le bouton
+    // Rediriger vers la page "nouveau festival"
+    window.location.href = "nouveauFestival.html";
+}
